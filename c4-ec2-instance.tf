a129740eac192bd78a8c234bef806f17fea1f197
Gopal <Gopal1409@gmail.com>
2021-08-25 02:59:43 -0700

    terraform provisoner

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami = data.aws_ami.amznlinux2.id
   #instance_type = var.instance_type
   #instance_type = var.instance_type_list[1] #for list
-  key-name = "terraform-key"
+  key_name = "terraform-key"
   instance_type = var.instance_type
   #count = terraform.workspace == "default" ? 2:1
   user_data = file("apache-install.sh")
