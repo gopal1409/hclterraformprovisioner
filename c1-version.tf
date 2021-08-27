e9ee3f1496652862630e7af5fd4c4312f8786262
Gopal <Gopal1409@gmail.com>
2021-08-27 02:53:38 -0700

    terraform provisioner

--- c1-version.tf
+++ c1-version.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+  bucket = "terraform-state-gopal"
+  key = "dev/terraform.tfstate"
+  region = "ap-northeast-2"
+  dynamodb_table = "gopal-ec2-project"
+ }
 }
 
 # Configure the AWS Provider
